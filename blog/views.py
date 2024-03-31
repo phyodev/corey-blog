375f6af6352f17a508ce62472bfc802f78f8f84e
stefon <stefon@visibleone.com>
2024-03-31 11:12:13 +0630

    feat[Profile]: create profile on user create using signal and Profile model added including media saving

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
+@login_required
 def home(request):
-    if not request.user.is_authenticated: return redirect('login')
+    # if not request.user.is_authenticated: return redirect('login')
     context = {
         'posts': Post.objects.all(),
         'title': 'Home'
     }
     return render(request, 'blog/home.html', context)
 
+@login_required
 def about(request):
-    if not request.user.is_authenticated: return redirect('login')
+    # if not request.user.is_authenticated: return redirect('login')
     context = {
         'title': 'About'
     }
